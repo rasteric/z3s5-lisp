1fb034f23ee8dfc46dc140bf5998c9cff36e2c5c
E. H. Rast <erich@snafu.de>
2022-08-02 09:52:38 +0100

    Added automatic build testing in the main directory. This requires presence of the z3 binary, though.

---
+++ lisp_test.go
@@ -0,0 +1,18 @@
+package z3s5
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+)
+
+func TestDistribution(t *testing.T) {
+	cmd := exec.Command("make", "test")
+	cmd.Dir = "cmd/z3"
+	_, err := cmd.Output()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			panic(fmt.Errorf("Lisp internal tests failed with error code %v", exitError.ExitCode()))
+		}
+	}
+}
